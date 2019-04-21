2da6e3d812c1b851326739545f450cf93a2a945e
royferd <roy.r.phys@gmail.com>
2019-04-21 16:33:28 -0400

    Adding Autosave version files, something got messed up and I'm trying to recover Friday's work

--- sort_state.m
+++ sort_state.m
@@ -18,6 +18,8 @@ function [lcm1_avg_trash_raw,lcm1_weight_avg_trash_raw,...
     start_point,end_point,lcm1_avg_raw,lcm1_weight_raw,time,num_rows,...
     numpoints,sampling_time,EDM_sim,power_supply)
 
+    disp('running sort_state.m');
+
     %%%%%%%%%%%%%%%%%%%%%%%% ramp test code %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %%08-28-2017 put this into github. implement identical code, both
     %%polarities.
@@ -36,18 +38,54 @@ function [lcm1_avg_trash_raw,lcm1_weight_avg_trash_raw,...
     % avg around 0. To separate HI/LO values from 0 values, ignore voltages
     % within pm 1 kV of vmon_avg_avg. HI = negative, LO= positive
 
-    for i = 1:num_files    
-        vmon_avg_avg(i) = mean(vmon_avg(i,start_point(i):end_point(i)));
-        vmon_avg_total_stdev(i) = std(vmon_avg(i,start_point(i):end_point(i)));
-        vmon_hi_vals = find(vmon_avg(i,:) < vmon_avg_avg(i) - 5);
-        vmon_lo_vals = find(vmon_avg(i,:) > vmon_avg_avg(i) + 5);
-        vmon_ramp_high_deviation(i) = std(vmon_avg(i,vmon_hi_vals),vmon_weight(i,vmon_hi_vals));
-        vmon_ramp_low_deviation(i) = std(vmon_avg(i,vmon_lo_vals),vmon_weight(i,vmon_lo_vals));
-        vmon_ramp_high(i) = sum(vmon_avg_wt(i,vmon_hi_vals))/sum(vmon_weight_raw(i,vmon_hi_vals));
-        vmon_ramp_low(i) = sum(vmon_avg_wt(i,vmon_lo_vals))/sum(vmon_weight_raw(i,vmon_lo_vals));    
+    
+    
+    for i = 1:num_files
+        
+        length_data = length(vmon_avg);
+        [vmon_avg_list,sorted_indices] = sort(vmon_avg(i,:));
+        vmon_wt_list = vmon_weight(i,sorted_indices);
+        vmon_wt_raw_list = vmon_weight_raw(i,sorted_indices);
+        vmon_avg_wt_list = vmon_avg_wt(i,sorted_indices);
+        
+        % look at the bottom 15% and top 15% to find the max and min
+        % voltages.
+        
+        minimum_voltages_index = floor(length_data*.20);
+        maximum_voltages_index = floor(length_data*.80);
+        
+%         vmon_avg_avg(i) = mean(vmon_avg(i,start_point(i):end_point(i)));
+%         vmon_avg_total_stdev(i) = std(vmon_avg(i,start_point(i):end_point(i)));
+%         vmon_hi_vals = find(vmon_avg(i,:) < vmon_avg_avg(i) - 5);
+%         vmon_lo_vals = find(vmon_avg(i,:) > vmon_avg_avg(i) + 5);
+        
+        % high negative voltage
+        vmon_hi_vals = vmon_avg_list(i,1:minimum_voltages_index);
+        vmon_hi_weights = vmon_wt_list(i,1:minimum_voltages_index);
+        vmon_hi_weights_raw = vmon_wt_raw_list(i,1:minimum_voltages_index);
+        vmon_hi_avg_wts = vmon_avg_wt_list(i,1:minimum_voltages_index);
+        
+        vmon_lo_vals = vmon_avg_list(maximum_voltages_index:end);
+        vmon_lo_weights = vmon_wt_list(maximum_voltages_index:end);      
+        vmon_lo_weights_raw = vmon_wt_raw_list(maximum_voltages_index:end);
+        vmon_lo_avg_wts = vmon_avg_wt_list(maximum_voltages_index:end);
+        
+%         vmon_ramp_high_deviation(i) = std(vmon_avg(i,vmon_hi_vals),vmon_weight(i,vmon_hi_vals));
+%         vmon_ramp_low_deviation(i) = std(vmon_avg(i,vmon_lo_vals),vmon_weight(i,vmon_lo_vals));
+%         vmon_ramp_high(i) = sum(vmon_avg_wt(i,vmon_hi_vals))/sum(vmon_weight_raw(i,vmon_hi_vals));
+%         vmon_ramp_low(i) = sum(vmon_avg_wt(i,vmon_lo_vals))/sum(vmon_weight_raw(i,vmon_lo_vals));  
+        
+%         vmon_ramp_high_deviation(i) = std(vmon_hi_vals,vmon_hi_weights);
+%         vmon_ramp_low_deviation(i) = std(vmon_lo_vals,vmon_lo_weights);
+                
+        
+        vmon_ramp_high(i) = sum(vmon_hi_avg_wts)/sum(vmon_hi_weights_raw);
+        vmon_ramp_low(i) = sum(vmon_lo_avg_wts)/sum(vmon_lo_weights_raw); 
 
     end
 
+    fprintf('high negative voltage = %f +/- %f kV \n',vmon_ramp_high(i),vmon_ramp_high_deviation(i));
+    fprintf('high positive voltage = %f +/- %f kV \n',vmon_ramp_low(i),vmon_ramp_low_deviation(i));
     %ramp_deviation = 0.05;
 
     time_ramp_up_pass = zeros(num_files,num_rows,1);
@@ -108,10 +146,15 @@ function [lcm1_avg_trash_raw,lcm1_weight_avg_trash_raw,...
 
             for j = start_point(i):numpoints(i)           
 
+%                 if abs(vmon_avg(i,j) - vmon_ramp_high(i)) < ...
+%                         3*vmon_ramp_high_deviation(i) && ...
+%                         vmon_avg(i,j) < ...
+%                         vmon_ramp_high(i) + vmon_ramp_high_deviation(i)
+                    
                 if abs(vmon_avg(i,j) - vmon_ramp_high(i)) < ...
                         3*vmon_ramp_high_deviation(i) && ...
                         vmon_avg(i,j) < ...
-                        vmon_ramp_high(i) + vmon_ramp_high_deviation(i)
+                        vmon_ramp_high(i) + 5*vmon_ramp_high_deviation(i)
 
                     index_up_chunk_data_pass(i,end+1) = j;
                     num_ramp_up_points(i) = num_ramp_up_points(i) + 1;
@@ -131,9 +174,12 @@ function [lcm1_avg_trash_raw,lcm1_weight_avg_trash_raw,...
 
                     end
 
-                elseif (abs(vmon_avg(i,j) - vmon_ramp_low(i)) < 3*vmon_ramp_low_deviation(i) && ...
-                        vmon_avg(i,j) > vmon_ramp_low(i) - vmon_ramp_low_deviation(i))
+%                 elseif (abs(vmon_avg(i,j) - vmon_ramp_low(i)) < 3*vmon_ramp_low_deviation(i) && ...
+%                         vmon_avg(i,j) > vmon_ramp_low(i) - vmon_ramp_low_deviation(i))
 
+                elseif (abs(vmon_avg(i,j) - vmon_ramp_low(i)) < 3*vmon_ramp_low_deviation(i) && ...
+                        vmon_avg(i,j) > vmon_ramp_low(i) - 5*vmon_ramp_low_deviation(i))
+                    
                     index_down_chunk_data_pass(i,end+1) = j;
                     
                     num_ramp_down_points(i) = num_ramp_down_points(i) + 1;
@@ -288,31 +334,30 @@ function [lcm1_avg_trash_raw,lcm1_weight_avg_trash_raw,...
 
         if EDM_sim == 1
 
-            file_number_str = sprintf('%d',i);
-            up_chunks_str = sprintf('%d',count_up_chunks(i)-2);
-            down_chunks_str = sprintf('%d',count_down_chunks(i)-2);
-            trash_chunks_str = sprintf('%d',count_trash_chunks(i)-4);
-            mes1 = ['file number: ',file_number_str];
-            mes2 = ['number of up_chunks: ', up_chunks_str];
-            mes3 = ['number of  down chunks: ', down_chunks_str];
-            mes4 = ['number of trash chunks: ', trash_chunks_str];
-            disp(mes1)
-            disp(mes2)
-            disp(mes3)
-            disp(mes4)
-            disp('the number of trash chunks should be the sum of up and down chunks')              
+%            file_number_str = sprintf('%d',i);
+%             up_chunks_str = sprintf('%d',num_up_chunks(i));
+%             down_chunks_str = sprintf('%d',num_down_chunks);
+%             trash_chunks_str = sprintf('%d',num_trash_chunks);
+%             mes1 = ['file number: ',file_number_str];
+%             mes2 = ['number of up_chunks: ', up_chunks_str];
+%             mes3 = ['number of  down chunks: ', down_chunks_str];
+%             mes4 = ['number of trash chunks: ', trash_chunks_str];
+%             disp(mes1)
+%             disp(mes2)
+%             disp(mes3)
+%             disp(mes4)
+%             disp('the number of trash chunks should be the sum of up and down chunks')                                                  
 
         end
 
     end
-
-
-    up_chunk_array = zeros(num_files,2,num_up_chunks); % this triggers an error
+    
+    up_chunk_array_pass_pass = zeros(num_files,1); % this triggers an error
     % if more than one file is being read
     
-    down_chunk_array = zeros(num_files,2,num_down_chunks);
+    down_chunk_array = zeros(num_files,2,1);
     
-    trash_chunk_array = zeros(num_files,2,num_trash_chunks);
+    trash_chunk_array_pass_pass = zeros(num_files,2,1);
 
     time_ramp_up = zeros(num_files,num_ramp_up_points,1);
 %    time_ramp_up = [];    
@@ -355,83 +400,6 @@ function [lcm1_avg_trash_raw,lcm1_weight_avg_trash_raw,...
 
     for i = 1:num_files
         
-        
-        %start at third trash chunk
-        for j =1:num_trash_chunks(i) + 1
-
-%             trash_chunk_array(i,1,j) = trash_chunk_array_pass(i,2,j+1) - extra - 1;
-%             
-%             trash_chunk_array(i,2,j) = trash_chunk_array_pass(i,2,j+2) + extra - 1;
-            
-            trash_chunk_array(i,1,j) = trash_chunk_array_pass(i,2,j+1);
-            
-            trash_chunk_array(i,2,j) = trash_chunk_array_pass(i,2,j+2);
-            
-            in_this_chunk = trash_chunk_array(i,2,j) - trash_chunk_array(i,1,j) - 1;
-            
-%             lcm1_avg_trash_raw(end+1:end+1+in_this_chunk) = ...
-%                 lcm1_avg_raw(i,index_trash_chunk_data(i,...
-%                 (trash_chunk_array(i,1,j)+1):trash_chunk_array(i,2,j)));
-% 
-%             lcm1_weight_avg_trash_raw(end+1:end+1+in_this_chunk) = ...
-%                 lcm1_weight_raw(i,index_trash_chunk_data(i,...
-%                 (trash_chunk_array(i,1,j)+1):trash_chunk_array(i,2,j)));
-%             
-%             time_trash(end+1:end+1+in_this_chunk) = ...
-%                 time(i,index_trash_chunk_data(i,...
-%                 (trash_chunk_array(i,1,j)+1):trash_chunk_array(i,2,j)));
-
-        end
-        
-        
-
-
-        %start at 2nd down chunk
-        for j =1:num_down_chunks(i) + 1
-
-            down_chunk_array(i,1,j) = down_chunk_array_pass(i,2,j) + extra;
-            
-            down_chunk_array(i,2,j) = down_chunk_array_pass(i,2,j+1) - extra;
-            
-            in_this_chunk = down_chunk_array(i,2,j) - down_chunk_array(i,1,j) -1;
-            
-%             lcm1_avg_ramp_down_raw(end+1:end+1+in_this_chunk) = ...
-%                 lcm1_avg_raw(i,index_down_chunk_data(i,(down_chunk_array(i,1,j)+1):down_chunk_array(i,2,j)));
-% 
-%             lcm1_weight_avg_ramp_down_raw(end+1:end+1+in_this_chunk) = ...
-%                 lcm1_weight_raw(i,index_down_chunk_data(i,(down_chunk_array(i,1,j)+1):down_chunk_array(i,2,j)));
-%             
-%             time_ramp_down(end+1:end+1+in_this_chunk) = ...
-%                 time(i,index_down_chunk_data(i,(down_chunk_array(i,1,j)+1):down_chunk_array(i,2,j)));
-
-
-        end
-        
-
-        
-        %start at 2nd up chunk
-        for j =1:num_up_chunks(i) + 1
-
-            up_chunk_array(i,1,j) = up_chunk_array_pass(i,2,j) + extra;
-            
-            up_chunk_array(i,2,j) = up_chunk_array_pass(i,2,j+1) - extra;
-
-            in_this_chunk = up_chunk_array(i,2,j) - up_chunk_array(i,1,j) -1;
-
-%             lcm1_avg_ramp_up_raw(end+1:end+1+in_this_chunk) = ...
-%                 lcm1_avg_raw(i,index_up_chunk_data(i,(up_chunk_array(i,1,j)+1):up_chunk_array(i,2,j)));
-% 
-%             lcm1_weight_avg_ramp_up_raw(end+1:end+1+in_this_chunk) = ...
-%                 lcm1_weight_raw(i,index_up_chunk_data(i,(up_chunk_array(i,1,j)+1):up_chunk_array(i,2,j)));
-%             
-%             time_ramp_up(end+1:end+1+in_this_chunk) = ...
-%                 time(i,index_up_chunk_data(i,(up_chunk_array(i,1,j)+1):up_chunk_array(i,2,j)));
-
-        end
-        
-
-        
-        
         for j = 1:num_ramp_up_points(i)
 
             lcm1_avg_ramp_up_raw(i,j) = lcm1_avg_ramp_up_raw_pass(i,j+1);
@@ -467,6 +435,234 @@ function [lcm1_avg_trash_raw,lcm1_weight_avg_trash_raw,...
             time_trash(i,j) = time_trash_pass(i,j+1);
 
         end
+        
+        %start at 2nd down chunk
+        for j =1:num_down_chunks(i) + 1
+
+            down_chunk_array(i,1,j) = down_chunk_array_pass(i,2,j) + extra;
+            
+            down_chunk_array(i,2,j) = down_chunk_array_pass(i,2,j+1) - extra;
+            
+            in_this_chunk = down_chunk_array(i,2,j) - down_chunk_array(i,1,j) -1;
+
+        end
+        
+        %start at 2nd up chunk
+        for j =1:num_up_chunks(i) + 1
+            
+            lookfor_start = up_chunk_array_pass(i,2,j) + extra;
+            
+            if time_ramp_up(i,lookfor_start+1) > time_ramp_down(i,down_chunk_array(i,1,1)+1)
+                
+                up_chunk_begin = j;
+                
+                break;
+                
+            end
+            
+        end
+        
+        
+        % now look for last up chunk, which should occur after the last
+        % down chunk. 
+        for j = up_chunk_begin:num_up_chunks(i) + 1
+            
+            lookfor_end = down_chunk_array_pass(i,2,j+1) - extra;
+            
+            fprintf('up chunk number = %d \n',j);
+            fprintf('lookfor_end = %d \n',down_chunk_array_pass(i,2,j+1) - extra);
+            fprintf('length of time_ramp_up = %d \n',length(time_ramp_up));
+
+            if time_ramp_up(i,lookfor_end) > time_ramp_down(i,down_chunk_array(i,2,num_down_chunks(i)))
+                
+                up_chunk_end = j;
+                
+                break;
+                
+            end
+            
+        end
+        
+        % If this does not happen, e.g. if the simulation isn't allowed to 
+        % finish its last cycle, then reduce the number of down chunks by
+        % the appropriate number of cycles.
+        if time_ramp_down(i,down_chunk_array(i,2,num_down_chunks(i))) > time_ramp_up(i,lookfor_end)
+            
+            up_chunk_end = num_up_chunks(i)+1;
+                
+            for j = 1:num_down_chunks(i)-1
+                
+                if time_ramp_down(i,down_chunk_array(i,2,num_down_chunks(i)-j)) < time_ramp_up(i,lookfor_end)
+                    
+                    num_down_chunks(i) = num_down_chunks(i) - j;
+                    
+                    break;
+                    
+                end
+                
+            end
+            
+        end
+        
+        num_up_chunks(i) = up_chunk_end - up_chunk_begin + 1;
+        
+        for j = up_chunk_begin:num_up_chunks(i)+1
+
+            up_chunk_array_pass_pass(i,1,end+1) = up_chunk_array_pass(i,2,j) + extra;
+            
+            up_chunk_array_pass_pass(i,2,end) = up_chunk_array_pass(i,2,j+1) - extra;
+
+            in_this_chunk = up_chunk_array_pass_pass(i,2,j) - up_chunk_array_pass_pass(i,1,j) -1;
+
+        end
+        
+
+        % first trash chunk starts before first down chunk. Last trash chunk
+        % ends after last up chunk.
+        for j =1:num_trash_chunks(i) + 1
+            
+            lookfor_start = trash_chunk_array_pass(i,2,j+1) + extra;
+            
+            if time_trash(i,lookfor_start+1) > time_ramp_down(i,down_chunk_array(i,1,1)+1) && j > 1
+                
+                trash_chunk_begin = j-1;
+                
+                break;
+                
+            end
+            
+        end
+        
+        % now look for last trash chunk, which should occur after the last
+        % up chunk. 
+        for j = trash_chunk_begin:num_trash_chunks(i) + 1
+            
+            lookfor_end = trash_chunk_array_pass(i,2,j+1) - extra;
+            
+            if time_trash(i,lookfor_end) > time_ramp_up(i,up_chunk_array_pass_pass(i,2,num_up_chunks(i)+1))
+                
+                trash_chunk_end = j;
+                
+                break;
+                
+            end
+            
+        end      
+        
+        % If this does not happen, e.g. if the simulation isn't allowed to 
+        % finish its last cycle, then reduce the number of up and down 
+        % chunks by the appropriate number of cycles.
+        if time_ramp_up(i,up_chunk_array_pass_pass(i,2,num_up_chunks(i)+1)) > ...
+                time_trash(i,lookfor_end)
+            
+            trash_chunk_end = num_trash_chunks(i)+1;
+                
+            for j = 1:num_up_chunks(i)-1
+                
+                if time_ramp_up(i,up_chunk_array_pass_pass(i,2,num_up_chunks(i)+1-j)) < ...
+                        time_trash(i,lookfor_end)
+                    
+                    num_up_chunks(i) = num_up_chunks(i) - j;
+                    num_down_chunks(i) = num_down_chunks(i) - j;
+                    
+                    break;
+                    
+                end
+                
+            end
+            
+        end
+        
+        num_trash_chunks(i) = trash_chunk_end - trash_chunk_begin + 1;       
+        
+        
+        for j =trash_chunk_begin:num_trash_chunks(i)+1
+            
+            trash_chunk_array_pass_pass(i,1,end+1) = trash_chunk_array_pass(i,2,j+1) + extra;
+            
+            trash_chunk_array_pass_pass(i,2,end) = trash_chunk_array_pass(i,2,j+2) + extra;
+            
+            in_this_chunk = trash_chunk_array_pass_pass(i,2,end) - trash_chunk_array_pass_pass(i,1,end) - 1;
+
+        end
+        
+        
+%         %start at third trash chunk
+%         for j =1:num_trash_chunks(i) + 1
+%             
+%             trash_chunk_array(i,1,j) = trash_chunk_array_pass(i,2,j+1);
+%             
+%             trash_chunk_array(i,2,j) = trash_chunk_array_pass(i,2,j+2);
+%             
+%             in_this_chunk = trash_chunk_array(i,2,j) - trash_chunk_array(i,1,j) - 1;
+% 
+%         end
+        
+        
+
+
+%         %start at 2nd down chunk
+%         for j =1:num_down_chunks(i) + 1
+% 
+%             down_chunk_array(i,1,j) = down_chunk_array_pass(i,2,j) + extra;
+%             
+%             down_chunk_array(i,2,j) = down_chunk_array_pass(i,2,j+1) - extra;
+%             
+%             in_this_chunk = down_chunk_array(i,2,j) - down_chunk_array(i,1,j) -1;
+% 
+%         end
+        
+
+        
+%         %start at 2nd up chunk
+%         for j =1:num_up_chunks(i) + 1
+% 
+%             up_chunk_array(i,1,j) = up_chunk_array_pass(i,2,j) + extra;
+%             
+%             up_chunk_array(i,2,j) = up_chunk_array_pass(i,2,j+1) - extra;
+% 
+%             in_this_chunk = up_chunk_array(i,2,j) - up_chunk_array(i,1,j) -1;
+% 
+%         end
+        
+
+        
+        
+%         for j = 1:num_ramp_up_points(i)
+% 
+%             lcm1_avg_ramp_up_raw(i,j) = lcm1_avg_ramp_up_raw_pass(i,j+1);
+%             lcm1_weight_avg_ramp_up_raw(i,j) = lcm1_weight_avg_ramp_up_raw_pass(i,j+1);
+%             lcm1_inv_weight_avg_ramp_up_raw(i,j) = (lcm1_weight_avg_ramp_up_raw(i,j))^(-1/2);
+%             
+%             time_ramp_up(i,j) = time_ramp_up_pass(i,j+1);
+% 
+%         end
+% 
+%        
+% 
+%         for j = 1:num_ramp_down_points(i)
+% 
+%             lcm1_avg_ramp_down_raw(i,j) = lcm1_avg_ramp_down_raw_pass(i,j+1);
+%             lcm1_weight_avg_ramp_down_raw(i,j) = lcm1_weight_avg_ramp_down_raw_pass(i,j+1);
+%             lcm1_inv_weight_avg_ramp_down_raw(i,j) = (lcm1_weight_avg_ramp_down_raw(i,j))^(-1/2);
+% 
+%             time_ramp_down(i,j) = time_ramp_down_pass(i,j+1);
+% 
+% 
+%         end
+% 
+% % 
+%         for j = 1:num_trash_points(i)
+% 
+%             lcm1_avg_trash_raw(i,j) = lcm1_avg_trash_raw_pass(i,j+1);
+%       
+%             lcm1_weight_avg_trash_raw(i,j) = lcm1_weight_avg_trash_raw_pass(i,j+1);
+%             
+%             lcm1_inv_weight_avg_trash_raw(i,j) = (lcm1_weight_avg_trash_raw(i,j))^(-1/2);
+%      
+%             time_trash(i,j) = time_trash_pass(i,j+1);
+% 
+%         end
 
 %         num_ramp_down_points(i) = length(lcm1_avg_ramp_down_raw);
 %         num_ramp_up_points(i) = length(lcm1_avg_ramp_up_raw);      
@@ -476,7 +672,16 @@ function [lcm1_avg_trash_raw,lcm1_weight_avg_trash_raw,...
 %        lcm1_inv_weight_avg_trash_raw = (lcm1_weight_avg_trash_raw(:,:)).^(-1/2);
 % % %         
 %          num_trash_points(i) = length(lcm1_avg_trash_raw);  
-%
+
     end
+    
+    up_chunk_array = up_chunk_array_pass_pass(:,:,2:end);
+    trash_chunk_array = trash_chunk_array_pass_pass(:,:,2:end);
+    
+    fprintf('file number: %d \n',i);
+    fprintf('number of up chunks (-V) = %d \n', num_up_chunks(i));
+    fprintf('number of  down chunks (+V) = %d \n', num_down_chunks(i));
+    fprintf('number of trash chunks (0V) = %d \n', num_trash_chunks(i));
+    disp('the number of trash chunks should be the sum of up and down chunks') 
     
 end
