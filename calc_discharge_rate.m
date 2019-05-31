241a17469ce4aa3997f9d5a3258754616f87aaa9
royferd <roy.r.phys@gmail.com>
2019-05-30 23:18:54 -0400

    checked that the code is compatible with the old MSU data, new MSU data, and ANL data. Major overhaul of calc_discharge_rate, which now correctly accounts for the live time of the voltage settings for calculating discharge rates. Also fixed a minor bug where chunks with zero discharges were counted as a discharge. Code is ready to be run over all the old niobium datasets for first pass global analysis of discharge rates.

--- calc_discharge_rate.m
+++ calc_discharge_rate.m
@@ -43,8 +43,9 @@ live_time = 0.0;
         dph_std = [1];
         overall_dph_std = 1;
         
-        median_discharge_size = [discharge_stdevs(1,1)];
-        overall_median_discharge_size = discharge_stdevs(1,1);
+        median_discharge_size = [discharge_vals(1,1)];
+        
+        overall_median_discharge_size = discharge_vals(1,1);
         
     elseif length(discharge_times(1,:)) > 1
         
@@ -52,8 +53,18 @@ live_time = 0.0;
             discharge_times(1,start_time_index));
                 
         overall_dph_std = sqrt(overall_dph);
+        
+        if isempty(find(discharge_vals(1,start_time_index:end),1))
+            
+            overall_median_discharge_size = 0.0;
+            
+        else
+            
+            [row, col, val] = find(discharge_vals(1,start_time_index:end));
 
-        overall_median_discharge_size = median(discharge_stdevs(1,start_time_index:end));
+            overall_median_discharge_size = median(val);
+            
+        end
     
         for i =1:length(discharge_times(1,:))
 
@@ -61,7 +72,19 @@ live_time = 0.0;
             
             time_delta = discharge_times(1,i) - start_time;
             
-            live_time = live_time + time_length_of_chunk(discharge_times(2,i)); 
+%            live_time = live_time + time_length_of_chunk(discharge_times(2,i)); 
+
+            if i==start_time_index
+                
+                live_time = live_time + ...
+                    sum(time_length_of_chunk(start_time_index:discharge_times(2,i)));
+            
+            elseif discharge_times(2,i) ~= discharge_times(2,i-1)
+                    
+                    live_time = live_time + ...
+                        sum(time_length_of_chunk(discharge_times(2,i-1)+1:discharge_times(2,i)));
+                                    
+            end
             
 %             time_since_last_hour = discharge_times(1,i) - first_discharge_time - 60.0*hour_number;
             
@@ -72,12 +95,14 @@ live_time = 0.0;
             % since the first discharge in this sequence of discharges, and
             % we compute a new discharge rate
             
-            if time_since_last_hour > 0.0       
+            if time_since_last_hour > 0.0 
                 
                 % if more than an hour passes between discharges, we need
                 % to record that zero discharges occurred
                 
-                if time_since_last_hour > 60.0
+%                 if time_since_last_hour > 60.0 
+                    
+                if time_since_last_hour > 60.0 && isempty(find(discharge_vals(1,start_index:i),1))
                     
                     while time_since_last_hour > 60.0
                         
@@ -89,11 +114,12 @@ live_time = 0.0;
 
                         median_discharge_size_this_hour = 0.0;
 
-                        dph = [dph avg_discharges_this_hour];
-
-                        dph_std = [dph_std std_this_hour];
-
-                        median_discharge_size = [median_discharge_size median_discharge_size_this_hour];
+%                         dph = [dph avg_discharges_this_hour];
+% 
+%                         dph_std = [dph_std std_this_hour];
+% 
+%                         median_discharge_size = ...
+%                             [median_discharge_size median_discharge_size_this_hour];
                         
                     end
                     
@@ -102,20 +128,36 @@ live_time = 0.0;
 
                     %discharges per hour
 
+%                     avg_discharges_this_hour = ...
+%                         length(discharge_times(1,start_time_index:i))*60.0/live_time;
+                    
                     avg_discharges_this_hour = ...
-                        length(discharge_times(1,start_time_index:i))*60.0/live_time;
+                        length(find(discharge_vals(1,start_time_index:i)))*60.0/live_time;
 
                     std_this_hour = sqrt(avg_discharges_this_hour);
 
-                    median_discharge_size_this_hour = ...
-                        median(discharge_stdevs(1,start_time_index:i));
+%                     median_discharge_size_this_hour = ...
+%                         median(discharge_vals(1,start_time_index:i));
+                    
 
-                    dph = [dph avg_discharges_this_hour];
+                    if isempty(find(discharge_vals(1,start_time_index:i),1))
+                        
+                        median_discharge_size_this_hour = 0.0;
+                        
+                    else
+                        
+                        [row, col, val] = find(discharge_vals(1,start_time_index:i));
+                       
+                        median_discharge_size_this_hour = median(val);
+                        
+                    end
 
-                    dph_std = [dph_std std_this_hour];
-
-                    median_discharge_size = ...
-                        [median_discharge_size median_discharge_size_this_hour];
+%                     dph = [dph avg_discharges_this_hour];
+% 
+%                     dph_std = [dph_std std_this_hour];
+% 
+%                     median_discharge_size = ...
+%                         [median_discharge_size median_discharge_size_this_hour];
                 
                 end
 
@@ -124,15 +166,15 @@ live_time = 0.0;
                 
                 % Break loop if we're at the last point                                
                 
-                if i == length(discharge_times(1,:))
+%                 if i == length(discharge_times(1,:))
+%                     
+% %                     start_time = discharge_times(1,i);
+% 
+% %                     start_time_index = i;
+%                     
+%                     break;
                     
-                    start_time = discharge_times(1,i);
-
-                    start_time_index = i;
-                    
-                    break;
-                    
-                else
+                if i ~= length(discharge_times(1,:))
                     
                     start_time = discharge_times(1,i);
 
@@ -142,7 +184,14 @@ live_time = 0.0;
                     
                     leftover_time = time_since_last_hour;
                     
-                    live_time = 0.0;                                        
+                    live_time = 0.0;
+                    
+                    dph = [dph avg_discharges_this_hour];
+
+                    dph_std = [dph_std std_this_hour];
+
+                    median_discharge_size = ...
+                        [median_discharge_size median_discharge_size_this_hour];
                     
                 end                                
 
@@ -150,46 +199,100 @@ live_time = 0.0;
 
         end
         
+        avg_discharges_this_hour = ...
+                length(find(discharge_vals(1,start_time_index:end)))*60/live_time;
+            
+        std_this_hour = sqrt(avg_discharges_this_hour);
+
+        if isempty(find(discharge_vals(1,start_time_index:end),1))
+
+            median_discharge_size_this_hour = 0.0;
+
+        else
+            
+            [row, col, val] = find(discharge_vals(1,start_time_index:end));
+                       
+            median_discharge_size_this_hour = median(val);
+
+        end           
+        
         % if we don't get a full last hour, we can extrapolate the last
         % dph
         
         time_delta = discharge_times(1,end) - start_time;
             
-        if time_since_last_hour < 0.0 && start_time_index ~= length(discharge_times(1,:)) && ...
-                time_delta > 8.0
+%         if time_since_last_hour < 0.0 && start_time_index ~= length(discharge_times(1,:)) && ...
+%                 time_delta > 8.0
             
-%             avg_discharges_this_hour = length(discharge_times(1,start_time_index:end))*60/time_delta;
+        if time_since_last_hour < 0.0  && time_delta > 15.0
+                       
+%             avg_discharges_this_hour = ...
+%                 length(discharge_times(1,start_time_index:end))*60/live_time;
             
-            avg_discharges_this_hour = length(discharge_times(1,start_time_index:end))*60/live_time;
-            
-            std_this_hour = sqrt(avg_discharges_this_hour);
-            
-            median_discharge_size_this_hour = median(discharge_stdevs(1,start_time_index:end));
-            
-            median_discharge_size = [median_discharge_size median_discharge_size_this_hour];
+%             avg_discharges_this_hour = ...
+%                 length(find(discharge_vals(1,start_time_index:end)))*60/live_time;
+%             
+%             std_this_hour = sqrt(avg_discharges_this_hour);
+%             
+%             if isempty(find(discharge_vals(1,start_time_index:end),1))
+%                 
+%                 median_discharge_size_this_hour = 0.0;
+%                 
+%             else
+%                                             
+%                 median_discharge_size_this_hour = ...
+%                     median(find(discharge_vals(1,start_time_index:end)));
+%                 
+%             end    
+                
+            median_discharge_size = ...
+                [median_discharge_size median_discharge_size_this_hour];
             
             dph = [dph avg_discharges_this_hour];
             
             dph_std = [dph_std std_this_hour];
             
-        elseif time_since_last_hour < 0.0 && start_time_index ~= length(discharge_times(1,:)) && ...
-                time_delta < 8.0
+%         elseif time_since_last_hour < 0.0 && start_time_index ~= length(discharge_times(1,:)) && ...
+%                 time_delta < 8.0
             
-%             avg_discharges_this_hour = length(discharge_times(1,start_time_index:end))*60.0/time_delta;
+        else
+          
+%             avg_discharges_this_hour = ...
+%                 length(discharge_times(1,start_time_index:end))*60.0/live_time;
             
-            avg_discharges_this_hour = length(discharge_times(1,start_time_index:end))*60.0/live_time;
-         
-            std_this_hour = sqrt(avg_discharges_this_hour);
-
-%             fprintf('start_time_index = %d \n',start_time_index);
-%             fprintf('length of discharge_stdevs = %d \n',length(discharge_stdevs));
+%             avg_discharges_this_hour = ...
+%                 length(find(discharge_vals(1,start_time_index:end)))*60.0/live_time;
+%          
+%             std_this_hour = sqrt(avg_discharges_this_hour);
+% 
+% %             fprintf('start_time_index = %d \n',start_time_index);
+% %             fprintf('length of discharge_stdevs = %d \n',length(discharge_stdevs));
+%             
+% %             median_discharge_size_this_hour = ...
+% %                 median([median_discharge_size(end) discharge_vals(1,start_time_index:end)]);
+%             
+% 
+            if isempty(find([median_discharge_size(end) discharge_vals(1,start_time_index:end)],1))
+                
+                median_discharge_size_this_hour = 0.0;
+                
+            else
+                
+                [row, col, val] = find([median_discharge_size(end) discharge_vals(1,start_time_index:end)]);
+                
+                median_discharge_size_this_hour = median(val);
+                
+            end
             
-            median_discharge_size_this_hour = ...
-                median([median_discharge_size(end) discharge_stdevs(1,start_time_index:end)]);
+%             dph(end) = (dph(end) + avg_discharges_this_hour)/2.0;
+%             
+%             dph_std(end) = sqrt((dph_std(end))^2+ (std_this_hour)^2);
+%             
+%             median_discharge_size(end) = median_discharge_size_this_hour;
             
-            dph(end) = (dph(end) + avg_discharges_this_hour)/2.0;
+            dph(end) = (dph(end) + (live_time/60.0)*avg_discharges_this_hour)/(1.0+live_time/60.0);
             
-            dph_std(end) = sqrt((dph_std(end))^2+ (std_this_hour)^2);
+            dph_std(end) = sqrt(dph_std(end));
             
             median_discharge_size(end) = median_discharge_size_this_hour;
     
