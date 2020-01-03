007c94e76f09ea1f03d5015fbc54ed6c3a0afb75
royferd <roy.r.phys@gmail.com>
2020-01-03 15:32:09 -0500

    ran 1/3/2020 data file.

--- RaEDM_leakage_analysis.m
+++ RaEDM_leakage_analysis.m
@@ -1,6 +1,6 @@
 % function RaEDM_leakage_analysis(datafile,power_supply)
 
-datafile = '2019-12-18-161830-hv-1.txt'; power_supply = 4;
+datafile = '2020-01-03-142124-hv-1.txt'; power_supply = 4;
 
 % histogram binning settings
 avg_binwidth = 5.0; % pA
