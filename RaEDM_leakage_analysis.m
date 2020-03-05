11284e6603101ea20f30d64d7a38a6a61715bd59
royferd <roy.r.phys@gmail.com>
2020-03-05 15:55:46 -0500

    ran 3/5/2020 data file and updated the timeline plot.

--- RaEDM_leakage_analysis.m
+++ RaEDM_leakage_analysis.m
@@ -1,6 +1,6 @@
 % function RaEDM_leakage_analysis(datafile,power_supply)
 
-datafile = '2020-02-24-145116-hv-1.txt'; power_supply = 4;
+datafile = '2020-03-05-145833-hv-1.txt'; power_supply = 4;
 
 % histogram binning settings
 avg_binwidth = 5.0; % pA
