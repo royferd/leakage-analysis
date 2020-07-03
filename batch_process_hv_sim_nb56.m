295fa9ff3994363302aba50e8131240e0cb4ddfd
Roy <roy.r.phys@gmail.com>
2020-07-02 20:24:15 -0400

    Created batch files for the remaining datasets, including a batch file that runs through all five datasets.

---
+++ batch_process_hv_sim_nb56.m
@@ -0,0 +1,20 @@
+clearvars -except masterlist binsize
+close all
+
+RaEDM_leakage_analysis('2017-10-16-180518-hv-1.txt',1); 
+ 
+RaEDM_leakage_analysis('2017-10-17-105238-hv-1.txt',1); 
+
+RaEDM_leakage_analysis('2017-10-17-165207-hv-1.txt',1); 
+
+RaEDM_leakage_analysis('2017-10-18-114452-hv-1.txt',1); 
+
+RaEDM_leakage_analysis('2017-10-18-171909-hv-1.txt',1); 
+
+RaEDM_leakage_analysis('2017-10-19-104525-hv-1.txt',1); 
+
+RaEDM_leakage_analysis('2017-10-19-171143-hv-1.txt',1); 
+
+RaEDM_leakage_analysis('2017-10-20-114432-hv-1.txt',1); 
+
+RaEDM_leakage_analysis('2017-10-23-175202-hv-1.txt',1); 
